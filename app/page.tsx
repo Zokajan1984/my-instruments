b4d5f94656ab48b694c88db1e05f33c4fb0f3ab1
Zokajan <zokirjon.astanov84@gmail.com>
2026-05-07 18:34:12 +0000

    7 мая 2026 г., 23:34

--- app/page.tsx
+++ app/page.tsx
@@ -53,7 +53,6 @@ export default async function Home() {
         <div className="flex justify-center">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-12 text-center ">
             {instruments?.map((item) => (
-              // ОСТАВЛЯЕМ ТОЛЬКО ЭТО — одна карточка на элемент
               <InstrumentCard
                 key={item.id}
                 item={item}
